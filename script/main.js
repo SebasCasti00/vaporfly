761ac3070a73c976f1f2230a5f1c45b6e972e08d
SebasCasti00 <sebas_castillo000826@hotmail.com>
2020-03-09 04:38:42 -0500

    gallery added

--- script/main.js
+++ script/main.js
@@ -6,3 +6,33 @@ function handleRotationInput () {
 }
 handleRotationInput();
 rotationInput.addEventListener('input', handleRotationInput);
+
+var btnRight = document.querySelector('.slider__btn--right');
+var btnLeft = document.querySelector('.slider__btn--left');
+var strip = document.querySelector('.slider__strip');
+var slider = document.querySelector('.slider');
+var posX = 0;
+var width = slider.offsetWidth;
+function handleBtnNextClick () {
+    
+    var quantity = strip.children.length;
+    if(posX > -width * (quantity - 1)){
+        posX -= width;
+    } else {
+        posX = 0;
+    }
+    strip.style.transform = 'translate(' + posX + 'px, 0px)';
+}
+btnRight.addEventListener('click', handleBtnNextClick);
+
+function handleBtnPrevClick () {
+    
+    var quantity = strip.children.length;
+    if(posX < 0){
+        posX += width;
+    } else {
+        posX = -width * (quantity - 1);
+    }
+    strip.style.transform = 'translate(' + posX + 'px, 0px)';
+}
+btnLeft.addEventListener('click', handleBtnPrevClick);
